a4ebd80dd84973a9073ee183cc0fcd2eeb528dd3
Timothy Abayo <timothy@tmc2.ai>
2024-06-15 20:19:45 +0300

    edited ReadMe&added requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Voice_Interaction_ATM
+# Voice_Interaction_ATM(VIA)
 A proof of concept simulating ATM speech interaction which can be used by blind people.
+# steps to run the VIA-API
+1. pip install -r requirements.py
+2. run uvicorn main:app --reload --port 8000
+3. you can test the VIA-API in the browser using localhost (127.0.0.1/8000)
--- speech_processor.py
+++ speech_processor.py
@@ -1,16 +1,42 @@
 import subprocess
 import speech_recognition as sr
+import pyttsx3
 
 class SpeechProcessor:
     def __init__(self):
         self.recognizer = sr.Recognizer()
+        
+        self.use_espeak = True
+        try:
+            subprocess.call(['which', 'espeak'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except FileNotFoundError:
+            self.use_espeak = False
+
+        if not self.use_espeak:
+            self.engine = pyttsx3.init()
+            self.engine.setProperty('rate', 150)  
+            self.engine.setProperty('volume', 1.0)  
 
     def text_to_speech(self, text):
-        subprocess.call(['espeak', text])
+        if self.use_espeak:
+            try:
+                subprocess.call(['espeak', text])
+            except Exception as e:
+                print(f"Error using espeak: {e}")
+                self.fallback_to_pyttsx3(text)
+        else:
+            self.fallback_to_pyttsx3(text)
+
+    def fallback_to_pyttsx3(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
 
     def speech_to_text(self, prompt=None):
         while True:
             with sr.Microphone() as source:
+                if prompt:
+                    print(prompt)
+                    self.text_to_speech(prompt)
                 print("Listening...")
                 self.recognizer.adjust_for_ambient_noise(source)
                 audio = self.recognizer.listen(source)
@@ -21,9 +47,8 @@ class SpeechProcessor:
                 return text
             except sr.UnknownValueError:
                 print("Sorry, I couldn't understand what you said. Please repeat.")
-                self.text_to_speech("Repeat what you said.")
+                self.text_to_speech("Sorry, I couldn't understand what you said. Please repeat.")
             except sr.RequestError as e:
-                print("Sorry, I couldn't request results from Google Speech Recognition service; {0}".format(e))
+                print(f"Sorry, I couldn't request results from Google Speech Recognition service; {e}")
                 return None
-
 
